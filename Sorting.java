d025b093155da1c46a2886352ab6bc7c6a3fa0eb
IvanDan <ivandanza@gmail.com>
2020-01-13 16:00:58 +0100

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,36 +1,10 @@
-import java.util.ArrayList;
-import java.util.Random;
 import static java.lang.System.*;
 
 public class Sorting {
-    static Random rand = new Random();
-    static ArrayList<Integer> num = new ArrayList<>();
-    static int nMax = 500000;
-    static int[] numRandom = new int[nMax];
-
-    private static void randNum() {
-        for (int i = 0; i < nMax; i++)
-            num.add(i + 1);
-        for (int i = 0; i < numRandom.length; i++) {
-            int size = num.size();
-            numRandom[i] = num.remove(rand.nextInt(size));
-        }
-    }
-
-    private static void arrayToString(int[] array) {
-        out.print("[");
-        for (int i = 0; i < array.length; i++)
-            out.print(i == array.length - 1 ? array[i] : (array[i] + ", "));
-        out.print("]");
-    }
-
-    private static void sort(int dim) {
-
-    }
 
     public static void main(String[] s) {
-        randNum();
+        int[] numRandom = Utils.cleanRandNum(10);
         // out.println(num.toString());
-        // arrayToString(numRandom);
+        Utils.printArray(numRandom);
     }
 }
