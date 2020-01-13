d10ae5ff7bf1537e3808b8018f94d2c4b67f73db
IvanDan <ivandanza@gmail.com>
2020-01-13 18:06:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] s) {
         int[] fullNumRandom = Utils.fullRandNum(10, 10);
+        out.println("array random");
         Utils.printArray(fullNumRandom);
         int[] arraySorted = Sorter.countingSort(fullNumRandom);
+        out.println("array ordinata");
         Utils.printArray(arraySorted);
     }
 }
