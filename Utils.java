98cf5fc05de2ac5b84907110690145b0d2006b2c
IvanDan <ivandanza@gmail.com>
2020-01-13 16:01:05 +0100

    Create Utils.java

---
+++ Utils.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Random;
+import static java.lang.System.*;
+
+public class Utils {
+
+    public static int[] cleanRandNum(int nMax) {
+        Random rand = new Random();
+        ArrayList<Integer> num = new ArrayList<>();
+        int[] numRandom = new int[nMax];
+        for (int i = 0; i < nMax; i++)
+            num.add(i + 1);
+        for (int i = 0; i < numRandom.length; i++) {
+            int size = num.size();
+            numRandom[i] = num.remove(rand.nextInt(size));
+        }
+        return numRandom;
+    }
+
+    public static void printArray(int[] array) {
+        out.print("[");
+        for (int i = 0; i < array.length; i++)
+            out.print(i == array.length - 1 ? array[i] : (array[i] + ", "));
+        out.print("]");
+    }
+}
