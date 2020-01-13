991c1d00a2538111f9e526a217b1853c94d7f023
IvanDan <ivandanza@gmail.com>
2020-01-13 19:57:31 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import static java.lang.System.*;
 public class Main {
 
     public static void main(String[] s) {
-        int[] fullNumRandom = Utils.fullRandNum(10, 10);
+        int[] fullNumRandom = Utils.fullRandNum(50, 50);
         out.println("array random");
         Utils.printArray(fullNumRandom);
         int[] arraySorted = Sorter.countingSort(fullNumRandom);
