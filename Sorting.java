96b8ef68270a433498d91dcb0026ea8ff4b8eb91
IvanDan <ivandanza@gmail.com>
2020-01-13 15:25:31 +0100

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -6,22 +6,23 @@ public class Sorting {
     static Random rand = new Random();
     static ArrayList<Integer> num = new ArrayList<>();
     static int nMax = 10;
-    static int[] num2 = new int[nMax];
+    static int[] numRandom = new int[nMax];
 
     private static void randNum() {
-        int aRandNum;
-        int countWhile;
-        aRandNum = rand.nextInt(nMax) + 1;
-        countWhile = 1;
-        while (num.size() != nMax) {
-            out.println("countWhile -> " + countWhile);
-            if (!num.contains(aRandNum)) {
-                num.add(aRandNum);
-            } else {
-                aRandNum = rand.nextInt(nMax) + 1;
-            }
-            countWhile++;
+        for (int i = 0; i < nMax; i++)
+            num.add(i + 1);
+        out.println(num.toString());
+        for (int i = 0; i < numRandom.length; i++) {
+            int size = num.size();
+            numRandom[i] = num.remove(rand.nextInt(size));
         }
+    }
+
+    private static void arrayToString(int[] array) {
+        out.print("[");
+        for (int i = 0; i < array.length; i++)
+            out.print(i == array.length - 1 ? array[i] : (array[i] + ", "));
+        out.print("]");
     }
 
     private static void sort(int dim) {
@@ -30,6 +31,6 @@ public class Sorting {
 
     public static void main(String[] s) {
         randNum();
-        out.println(num.toString());
+        arrayToString(numRandom);
     }
 }
